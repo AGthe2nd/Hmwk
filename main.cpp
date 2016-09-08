fb7c559326c127375a835d511fe022d08ba76c5e
AGthe2nd <bagalvanthe2nd@Gmail.com>
2016-09-08 15:08:49 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Student: Albert Galvan
+ * Instructor: Dr. Mark E. Lehr
+ * Created on September 8, 2016, 2:51 PM
+ * Purpose:  Answering Question from Savitch textbook
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes 
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    
+    //Declaration of Variables 
+    
+    //Input or initialize values 
+    
+    //Process/Calculations 
+    
+    //Output Located Here
+    cout<<"Note: The question comes from Savitch Chapter 1.4"<<endl;
+    cout<<"What are the types of Errors caught by Compliers?"<<endl;
+    cout<<"Syntax Errors"<<endl;
+
+
+    //Exit
+    return 0;
+}
